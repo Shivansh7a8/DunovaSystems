d1fd9eddfdd5123267f42b8d75c2eb87c685c1a3
Shivansh Shukla <89889494+Shivansh7a8@users.noreply.github.com>
2026-07-10 19:17:36 +0530

    Update contact.page.ts

--- website/src/app/pages/contact/contact.page.ts
+++ website/src/app/pages/contact/contact.page.ts
@@ -31,7 +31,7 @@ export class ContactPage {
   popupMessage = '';
 
   // Change this when deployed
-  api = "/api/contact";
+  api = "https://dunovasystems.onrender.com/api/contact";
 
   constructor(
     private fb: FormBuilder,
